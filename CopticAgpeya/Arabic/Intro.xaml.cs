dd44905fd4025c1dd0f2882a1cf72d55a60019b2
Marco Botros <btsmarco@gmail.com>
2012-12-25 15:23:41 +0400

    Done with all arabic files and about page. No settings page need now

--- CopticAgpeya/Arabic/Intro.xaml.cs
+++ CopticAgpeya/Arabic/Intro.xaml.cs
@@ -19,5 +19,50 @@ namespace CopticAgpeya.Arabic
         {
             InitializeComponent();
         }
+
+        private void Prime_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Arabic/Prime.xaml", UriKind.Relative));
+        }
+
+        private void Terce_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Arabic/Terce.xaml", UriKind.Relative));
+        }
+
+        private void Sext_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Arabic/Sext.xaml", UriKind.Relative));
+        }
+
+        private void None_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Arabic/None.xaml", UriKind.Relative));
+        }
+
+        private void Vespers_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Arabic/Vespers.xaml", UriKind.Relative));
+        }
+
+        private void Compline_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Arabic/Compline.xaml", UriKind.Relative));
+        }
+
+        private void Midnight_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Arabic/Midnight.xaml", UriKind.Relative));
+        }
+
+        private void Other_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Arabic/Prayers.xaml", UriKind.Relative));
+        }
+
+        private void Veil_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Arabic/Veil.xaml", UriKind.Relative));
+        }
     }
 }
